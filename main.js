c4d0b9dea02736a4202348019e3fe97a41ebbd59
Oslikovskyi <v.oslikovskiy@gmail.com>
2016-12-12 10:17:11 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var x1 = 66;
+var y1 = 666;
+var x2 = parseInt (prompt("enter your x2 coordinat: "));
+var y2 = parseInt (prompt("enter your y2 coordinat: "));
+var result = Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
+alert("The distance between points A(66,666) and B(" + x2 + "," + y2 + ")  is " + result);
